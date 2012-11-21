fbdaed85e236f07c435db215d120763e6abf3fe8
Johnny <developer94@gmail.com>
2012-11-21 23:08:56 +0100

    Example commit, changed "Skraceni" in the checkbox text to "Skraceni casovi".

--- application/views/raspored_view.php
+++ application/views/raspored_view.php
@@ -131,7 +131,7 @@
 			<option value="1">Prva</option>
 			<option value="2">Druga</option>
 		</select>
-		<input type="checkbox" id="skraceni">Skraceni</input>
+		<input type="checkbox" id="skraceni">Skraceni casovi</input>
 	</form></br>
 	<a id="izmeniLink" href="#">izmeni raspored</a>
 	</div>
